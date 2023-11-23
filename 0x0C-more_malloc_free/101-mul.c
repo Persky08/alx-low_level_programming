ea025deafd59aa891c4b2bd7bcfa22b149707d73
Persky08 <pesky08@gmail.com>
2023-11-23 20:24:19 +0000

    challenging

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,12 +8,33 @@ int is_digit(char *str);
 int _atoi(char *str);
 int multiply(char *str, char *str1);
 void errors(void);
+int _putchar(char c);
+
 /**
- * is_digit - a function that checks a string contains only digits
- * @str: string to be evaluated
+ * main - a function that multiplies to number
+ * @argc: argument counts
+ * @argv: argument vector
  *
  * Return: 0.
  */
+int main(int argc, char *argv[])
+{
+	int product;
+
+	if ((argc != 3) || !is_digit(argv[1]) || !is_digit(argv[2]))
+	{
+		errors();
+	}
+	product = multiply(argv[1], argv[2]);
+	return (product);
+}
+/**
+ * is_digit - a function that checks if a sting contains onlyy digits
+ *  @str: string to be evaluated
+ *
+ * Return: 0
+ */
+
 int is_digit(char *str)
 {
 	int i = 0;
@@ -58,7 +79,12 @@ int _atoi(char *str)
  */
 void errors(void)
 {
-	printf("Error\n");
+	_putchar('E');
+	_putchar('r');
+	_putchar('r');
+	_putchar('o');
+	_putchar('r');
+	_putchar('\n');
 	exit(98);
 }
 /**
@@ -77,22 +103,17 @@ int multiply(char *str, char *str1)
 	results = x * y;
 	return (results);
 }
-/**
- * main - main function to multiply to positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: product of two numbers
- */
-int main(int argc, char *argv[])
-{
-	int product;
 
-	if ((argc != 3) || !is_digit(argv[1]) || !is_digit(argv[2]))
-	{
-		errors();
-	}
-	product = multiply(argv[1], argv[2]);
-	printf("The product is %d\n", product);
-	return (0);
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
