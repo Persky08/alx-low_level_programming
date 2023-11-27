c4d557d1a52fa6be75aa052fda11f848123e5ea6
Persky08 <pesky08@gmail.com>
2023-11-27 16:42:04 +0000

    Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	_putchar("_");
-	_putchar(F);
-	_putchar(I);
-	_putchar(L);
-	_putchar(E);
-	_putchar(_);
+	_putchar('_');
+	_putchar('F');
+	_putchar('I');
+	_putchar('L');
+	_putchar('E');
+	_putchar('_');
 }
 
