8e99b946ee68ccd0ac8aad407cc4dffa01c9ef87
Persky08 <pesky08@gmail.com>
2023-11-27 16:27:52 +0000

    Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - a program that print the name of the file
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar("_");
+	_putchar(F);
+	_putchar(I);
+	_putchar(L);
+	_putchar(E);
+	_putchar(_);
+}
+
