c2c6d76a312a5b9cac2cf9b4d37d635983efe20d
Persky08 <pesky08@gmail.com>
2023-11-27 19:54:11 +0000

    structures and typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,2 +1,24 @@
 #include "dog.h"
 
+/**
+ * new_dog - a function that creates new dog
+ * @name: name of the new dog
+ * @age: age of the new dog
+ * @owner: owner of the new dog
+ *
+ * Return: void
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * free_dog - a function that frees dog
+ * @d: pointer to dog
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d.name);
+		free(d.owner);
+		free(d.age);
+	}
