16d5176c60bdd9c23784b3ba16d63bc1de1cdcc2
Persky08 <pesky08@gmail.com>
2023-12-12 09:55:54 +0000

    singly linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,12 +9,14 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t elements = 0;
+	const listint_t list = h;
 
 	while (h != NULL)
 	{
-		printf("%i\n", h->n);
-		h = h->next;
+		printf("%i\n", list->n);
 		elements++;
+		list = list->next;
+		
 	}
 	return (elements);
 }
