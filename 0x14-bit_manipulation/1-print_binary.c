e76e46c75df5f60dafb17bcfd0926a45b8b8a15e
Persky08 <pesky08@gmail.com>
2023-12-14 21:23:37 +0000

    used recursion

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_binary - a function that prints binary numbers
+ * @n: number to be converted
+ *
+ * Return: binary representation of the number
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (!n || n == '\0')
+	{
+		return;
+	}
+	if (n > 1)
+	{
+		/* Recursively print the binary representation of the quotient */
+		print_binary(n >> 1);
+	}
+	/* Print the last bit */
+	_putchar((n & 1) + '0');
+}
