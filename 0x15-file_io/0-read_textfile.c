251443567fd9611e94240c35f058fc385c5837b2
Persky08 <pesky08@gmail.com>
2023-12-21 11:02:42 +0000

    a function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,34 +7,36 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        ssize_t bytes_read, written_bytes;
-        char *buffer;
-        int file_descriptor;
+	ssize_t bytes_read, written_bytes;
+	char *buffer;
+	int file_descriptor;
 
-        if (filename == NULL)
-                return (0);
-        file_descriptor = open(filename, O_RDONLY);
-        if (file_descriptor == -1)
-                return (0);
-        buffer = malloc(sizeof(char) * letters);
-        if (buffer == NULL)
+	if (filename == NULL)
+		return (0);
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
         {
-                close(file_descriptor);
-                return (0);
-        }
-        bytes_read = read(file_descriptor, buffer, letters);
-        if (bytes_read == -1){
-                free(buffer);
-                close(file_descriptor);
-                return (0);}
-        written_bytes = write(STDOUT_FILENO, buffer, bytes_read);
-        if (written_bytes == -1 || written_bytes != bytes_read)
-        {
-                free(buffer);
-                close(file_descriptor);
-                return (0);
-        }
-        free(buffer);
-        close(file_descriptor);
-        return (written_bytes);
+		close(file_descriptor);
+		return (0);
+	}
+	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
+	written_bytes = write(STDOUT_FILENO, buffer, bytes_read);
+	if (written_bytes == -1 || written_bytes != bytes_read)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
+	free(buffer);
+	close(file_descriptor);
+	return (written_bytes);
 }
