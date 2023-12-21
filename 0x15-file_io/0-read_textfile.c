6ea7dae578d4891c279edd4e17de40dafd775b7b
Persky08 <pesky08@gmail.com>
2023-12-21 09:57:57 +0000

    edited

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,36 @@
 #include "main.h"
+
 /**
- * read_textfile - a function that reads text files and print to POIX
- * @filename: name of the file to be read
- * @letters: number of letters in the file
- * Return: number of letters
+ * read_textfile - a function that reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t bytes_read, written_bytes;
-	char *buffer;
 	int file_descriptor;
+	ssize_t read_file, written;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-	file_descriptor = open(filename, O_RDONLY);
+
+	fd = open(filename, O_RDONLY);
+
 	if (file_descriptor == -1)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(file_descriptor);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buffer)
 		return (0);
-	}
-	bytes_read = read(file_descriptor, buffer, letters);
-	if (bytes_read == -1)
-		free(buffer);
-		close(file_descriptor);
-		return (0);
-	written_bytes = write(STDOUT_FILENO, buffer, bytes_read);
-	if (written_bytes == -1 || written_bytes != bytes_read)
-	{
-		free(buffer);
-		close(file_descriptor);
-		return (0);
-	}
-	free(buffer);
+
+	nrd = read(file_descriptor, buffer, letters);
+	nwr = write(STDOUT_FILENO, buffer, read_file);
+
 	close(file_descriptor);
-	return (written_bytes);
+
+	free(buffer);
+
+	return (written);
 }
