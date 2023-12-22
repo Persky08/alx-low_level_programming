c81529b2ca0477a3e69ab7bb1b83f676037eb67d
Persky08 <pesky08@gmail.com>
2023-12-22 13:19:11 +0000

    Linux cp command

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,46 +1,5 @@
 #include "main.h"
-/**
- * main - a function that copies one file to another
- * @argc: number of arguments
- * @argv: Vector of arguments
- * @file_from: original file
- * @file_to: destination of the copy
- * @argv: vector of arguments
- * Return: copy files
- */
-void print_error(int file_from, int file_to, char *argv[]);
-int main(int argc, char *argv[])
-{
-	int file_to, file_from;
-	ssize_t bytes_read, written_bytes;
-	char buffer[1024];
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-		exit(97);
-	}
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Can't read from %s\n", argv[1]);
-		exit(98);
-	}
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	print_error(file_from, file_to, argv);
-	while ((bytes_read = read(file_from, buffer, 1024)) > 0)
-	{
-		written_bytes = write(file_to, buffer, bytes_read);
-		if (written_bytes == -1 || written_bytes != bytes_read)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(file_from);
-			close(file_to);
-			exit(99);
-		}
-	}
-	return (0);
-}
 
 /**
  * print_error - a function that prints error messages
@@ -52,7 +11,6 @@ int main(int argc, char *argv[])
  */
 void print_error(int file_from, int file_to, char *argv[])
 {
-
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
@@ -77,4 +35,48 @@ void print_error(int file_from, int file_to, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close %d\n", file_to);
 		exit(100);
 	}
+}
+
+/**
+ * main - a function that copies one file to another
+ * @argc: number of arguments
+ * @argv: Vector of arguments
+ *
+ * Return: copy files
+ */
+
+int main(int argc, char *argv[])
+{
+	int file_to, file_from;
+	ssize_t bytes_read, written_bytes;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Can't read from %s\n", argv[1]);
+		exit(98);
+	}
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
+	{
+		print_error(file_from, file_to, argv);
+	}
+	while ((bytes_read = read(file_from, buffer, 1024)) > 0)
+	{
+		written_bytes = write(file_to, buffer, bytes_read);
+		if (written_bytes == -1 || written_bytes != bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(file_from);
+			close(file_to);
+			exit(99);
+		}
+	}
+	return (0);
 }
