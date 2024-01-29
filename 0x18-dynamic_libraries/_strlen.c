1ba17855373b423f08a23b888a5d2053e5935812
Persky08 <pesky08@gmail.com>
2024-01-29 19:54:17 +0000

    new

--- 0x18-dynamic_libraries/_strlen.c
+++ 0x18-dynamic_libraries/_strlen.c
@@ -1,1 +1,18 @@
+#include "main.h"
 
+/**
+ * _strlen- returns length of string
+ * @s: string
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (*s != '\0')
+	{
+		a++;
+		s++;
+	}
+	return (a);
+}
