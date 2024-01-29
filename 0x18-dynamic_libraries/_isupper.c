b7b0365a8d050404bc8899cb4437a01db6783f2e
Persky08 <pesky08@gmail.com>
2024-01-29 20:16:33 +0000

    new

--- 0x18-dynamic_libraries/_isupper.c
+++ 0x18-dynamic_libraries/_isupper.c
@@ -1,1 +1,14 @@
+#include "main.h"
 
+/**
+ * _isupper - a function to check if a letter is uppercase
+ * @c: the character to check
+ *
+ * Return: always 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
